84ceb87947545178f586839a9380efd70c903f6f
Anthony Nguyen <59070271+antnguyen33@users.noreply.github.com>
2025-02-28 23:58:05 +0900

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+  import "$lib/css/global.css";
+  import SidebarToggle from "$lib/components/SidebarToggle.svelte";
+  import Sidebar from "$lib/components/Sidebar.svelte";
+  import { isSidebarOpen } from "$lib/stores";
+  import Header from "$lib/components/Header.svelte";
+</script>
+
+<div class="wrapper overflow-hidden">
+  <Sidebar />
+  <main class:show={$isSidebarOpen}>
+    <SidebarToggle />
+    <Header />
+    <div class="container">
+      <slot />
+    </div>
+  </main>
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    overflow: hidden;
+  }
+
+  main {
+    width: 100%;
+  }
+
+  .container {
+    margin: 2rem auto;
+  }
+</style>
